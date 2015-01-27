79dfbed449bfed6b958b157d080853f958dffe50
Dennis Yang <sunyang730@gmail.com>
2015-01-26 17:57:22 -0800

    - Completed the stringifyJSON test.

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,70 +4,62 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
-  var string = '';
-  if(obj == null){
-  	return 'null';
-  }
-  var key = Object.keys(obj);
-
-
-  var joinString = function(index, type){
-    if(index>key.length-2){
-      if(type == 'array'){
-      	if(typeof(obj[key[index]]) == 'array'){
-      		var key = Object.keys(obj[key[index]]);
-
-
-      	}else if(typeof(obj[key[index]]) != 'string'){
-          return obj[key[index]];
-        }else{
-          return '"' + obj[key[index]] + '"';
-        }
-      }else if(type == 'object'){
-        if(obj[key[index]] !='string' || 'number'){
-          return '';
-        }else if(typeof(obj[key[index]]) != 'string'){
-          return '"' + key[index] + '":' + obj[key[index]];
-        }else{
-          return '"' + key[index] + '":' + '"' + obj[key[index]] + '"';
-        }
-      }
-
-    }
-    if(type == 'array'){
-      if(typeof(obj[key[index]]) != 'string'){
-        return obj[key[index]] +','+ joinString(index+1, 'array');
-      }else{
-        return '"' + obj[key[index]] + '"' +  ',' + joinString(index+1, 'array');
-      }
-    }else if(type == 'object'){
-      if(obj[key[index]] != 'string' || 'number'){
-        return joinString(index+1, 'object');
-
-      }else if(typeof(obj[key[index]]) != 'string'){
-        return '"' + key[index] + '":' + obj[key[index]] + ',' + joinString(index+1, 'object');
-      }else{
-        return '"' + key[index] + '":' + '"' + obj[key[index]] + '"' + ',' + joinString(index+1, 'object');
-      }
-    }
-  };
-
-  if(Array.isArray(obj) && key.length !=0){
-    return string += '[' + joinString(0, 'array') + ']';
-  }else if(Array.isArray(obj)){
-  	return string += '[]';
-  }
-
-  if(typeof(obj) == 'object' && key.length !=0 ){
-    return string += '{' + joinString(0, 'object') + '}';
-  }else if(typeof(obj) == 'object'){
-    return string +='{}';
-  }
-  if(typeof(obj) == "string"){
-    return string += '"'+obj+'"';
-  }else{
-    return string += obj;
-  }
-
+	//string will be adding all the strings together. 
+	//key is to create a key array for the obj (either array or object).
+	var string = '', key;
+	//Check to see if obj is null if so, stop and return.
+	if(obj == null){
+		return string += 'null';
+	}
+	//Check to see if obj is a string, if so, just return the string with "".
+	if(typeof(obj) == 'string'){
+		return string += '"' + obj + '"';
+	}
+	//Check to see if obj is anything but an object, if so, return directly.
+	//If it is an object, then set the key for the object.
+	if(typeof(obj) != 'object'){
+		return string += obj;
+	}else{
+		key = Object.keys(obj);
+	}
+	//If there is nothing in the key then set it according to object or array.
+	if(key.length == 0){
+		if(Array.isArray(obj)){
+			return string += '[]';	
+		}else{
+			return string += '{}';
+		}
+		
+	}
+	//For loop through every elements of the object. 
+	for(var i = 0; i < key.length; i++){
+		//if the object contains function or undefined value, then continue to the next
+		//iteration and do nothing. 
+		if(typeof(obj[key[i]]) == 'function' || typeof(obj[key[i]]) == 'undefined')
+		{
+			continue; 
+		}
+		//if key is a number, it means that it is an array. Setup the array format.
+		if(!isNaN(key[i])){
+			if(i < key.length-1){ 
+				string += stringifyJSON(obj[key[i]]) +',';
+			}else{
+				string += stringifyJSON(obj[key[i]]);
+			}	
+		//if key is not a number, it means it is an object, Setup the object format.
+		}else{
+			if(i < key.length-1){ 
+				string += '"'+ key[i]+ '":' + stringifyJSON(obj[key[i]]) +',';
+			}else{
+				string += '"'+ key[i]+ '":' + stringifyJSON(obj[key[i]]);
+			}	
+		}
+	}
+	//return the string according to its type. Object or array 
+	if(Array.isArray(obj)){
+		return '[' + string + ']';	
+	}else{
+		return '{' + string + '}';
+	}
+	
 };
